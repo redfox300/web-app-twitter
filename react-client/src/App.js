205ed992f71d00fa7eac5aa785685f23ab366314
Ioan Cioca <ioancioca@yahoo.ca>
2018-07-22 15:59:19 -0400

    Updated react app with components.

--- react-client/src/App.js
+++ react-client/src/App.js
@@ -1,21 +1,36 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import Grid from '@material-ui/core/Grid'
+import './App.css'
+import NavBar from './components/NavBar'
+import Column from './components/Column'
+
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+
+    render() {
+        return (
+            <React.Fragment>
+                <CssBaseline />
+                <NavBar />
+                <Grid container alignItems={"center"} direction={"column"} justify={"center"}>
+                    <Grid item>
+                        <Grid container alignItems={"flex-start"} justify={"center"} direction={"row"}>
+                            <Grid item>
+                                <Column screenName={"appdirect"}/>
+                            </Grid>
+                            <Grid item>
+                                <Column screenName={"techcrunch"}/>
+                            </Grid>
+                            <Grid item>
+                                <Column screenName={"laughingsquid"}/>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </React.Fragment>
+        );
+    }
 }
 
 export default App;
