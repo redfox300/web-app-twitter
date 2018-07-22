657a20856bc5a7fb54d5f88bc089bffef23e6407
Ioan Cioca <ioancioca@yahoo.ca>
2018-07-22 15:28:15 -0400

    Modified Column component with state to fetch tweet data. #2

--- react-client/src/components/Column.js
+++ react-client/src/components/Column.js
@@ -4,6 +4,7 @@ import List from '@material-ui/core/List'
 import ListItem from '@material-ui/core/ListItem'
 import PropTypes from 'prop-types'
 import { withStyles } from '@material-ui/core/styles'
+import * as twitterProxy from './../data/twitter'
 
 const styles = {
     tweetElement: {
@@ -13,25 +14,39 @@ const styles = {
 };
 
 class Column extends React.Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            screenName : props.screenName,
+            tweets: null
+        }
+    }
+    componentWillMount(){
+        //Fetch tweets from proxy and set them in component state
+        twitterProxy.getBatchOfTweets(this.state.screenName).then((data)=> this.setState({tweets : data}));
+    }
     render(){
         const { classes } = this.props;
+
         return(
-            <div>
-                <List>
-                    {this.props.tweets.map(tweet => {
-                        let myTweet = tweet;
-                        //Handle retweets
-                        if(tweet.retweeted_status){
-                            myTweet = tweet.retweeted_status;
-                        }
-                        return(
-                            <ListItem key={tweet.id_str} className={classes.tweetElement}>
-                                <Tweet tweet={myTweet}/>
-                            </ListItem>
-                        )
-                    })}
-                </List>
-            </div>
+            //Only render when tweets are available
+            (this.state.tweets ?
+                (<div>
+                    <List>
+                        {this.state.tweets.map(tweet => {
+                            let myTweet = tweet;
+                            //Handle retweets as it's own object
+                            if(tweet.retweeted_status){
+                                myTweet = tweet.retweeted_status;
+                            }
+                            return(
+                                <ListItem key={tweet.id_str} className={classes.tweetElement}>
+                                    <Tweet tweet={myTweet}/>
+                                </ListItem>
+                            )
+                        })}
+                    </List>
+                </div>) :null)
         )
     }
 }
