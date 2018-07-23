66c2b40e7f3ab1d7b39deeda3a5add6f67762626
Ioan Cioca <ioancioca@yahoo.ca>
2018-07-22 21:31:39 -0400

    Added breakpoints for mobile view and state for tab selection. #6

--- react-client/src/App.js
+++ react-client/src/App.js
@@ -4,33 +4,61 @@ import Grid from '@material-ui/core/Grid'
 import './App.css'
 import NavBar from './components/NavBar'
 import Column from './components/Column'
-
+import withWidth, { isWidthUp } from '@material-ui/core/withWidth'
 
 class App extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            activeTabValue : 0,
+            names : ["appdirect", "techcrunch", "laughingsquid"]
+        }
+    }
+
+    handleTabChange = (event, value) => {
+        this.setState({ activeTabValue: value});
+    };
 
     render() {
-        return (
-            <React.Fragment>
-                <CssBaseline />
-                <NavBar />
-                <Grid container alignItems={"center"} direction={"column"} justify={"center"}>
-                    <Grid item>
-                        <Grid container alignItems={"flex-start"} justify={"center"} direction={"row"}>
-                            <Grid item>
-                                <Column screenName={"appdirect"}/>
-                            </Grid>
-                            <Grid item>
-                                <Column screenName={"techcrunch"}/>
-                            </Grid>
-                            <Grid item>
-                                <Column screenName={"laughingsquid"}/>
+        console.log(this.props.width);
+        if(isWidthUp('lg', this.props.width)){
+            return (
+                <React.Fragment>
+                    <CssBaseline />
+                    <NavBar />
+                    <Grid container alignItems={"center"} direction={"column"} justify={"center"}>
+                        <Grid item>
+                            <Grid container alignItems={"flex-start"} justify={"center"} direction={"row"}>
+                                <Grid item>
+                                    <Column screenName={this.state.names[0]}/>
+                                </Grid>
+                                <Grid item>
+                                    <Column screenName={this.state.names[1]}/>
+                                </Grid>
+                                <Grid item>
+                                    <Column screenName={this.state.names[2]}/>
+                                </Grid>
                             </Grid>
                         </Grid>
                     </Grid>
-                </Grid>
-            </React.Fragment>
-        );
+                </React.Fragment>
+            );
+        } else {
+            return(
+                <React.Fragment>
+                    <CssBaseline />
+                    <NavBar tabs={{state: this.state, onChange: this.handleTabChange}}/>
+                    <Grid container alignItems={"center"} justify={"center"}>
+                        <Grid item>
+                            {this.state.activeTabValue === 0 && <Column screenName={this.state.names[0]}/>}
+                            {this.state.activeTabValue === 1 && <Column screenName={this.state.names[1]}/>}
+                            {this.state.activeTabValue === 2 && <Column screenName={this.state.names[2]}/>}
+                        </Grid>
+                    </Grid>
+                </React.Fragment>
+            );
+        }
     }
 }
 
-export default App;
+export default withWidth()(App);
