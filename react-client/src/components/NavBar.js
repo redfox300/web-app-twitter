e4cf9465b72ed3f99ca104030badfa0750d0d68f
Ioan Cioca <ioancioca@yahoo.ca>
2018-07-22 21:09:42 -0400

    Updated NavBar component to render tabs depending on props. #6

--- react-client/src/components/NavBar.js
+++ react-client/src/components/NavBar.js
@@ -2,8 +2,10 @@ import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
 
-const NavBar = () => {
+const NavBar = (props) => {
     return(
         <div>
             <AppBar position="static" color={"primary"}>
@@ -12,8 +14,15 @@ const NavBar = () => {
                         Twitter Tweet Viewer
                     </Typography>
                 </Toolbar>
+                {(props.tabs ?
+                (<Tabs value={props.tabs.state.activeTabValue} onChange={props.tabs.onChange} fullWidth>
+                    <Tab value={0} label={props.tabs.state.names[0]} />
+                    <Tab value={1} label={props.tabs.state.names[1]} />
+                    <Tab value={2} label={props.tabs.state.names[2]} />
+                </Tabs>): null)}
             </AppBar>
         </div>
     )
-}
+};
+
 export default NavBar;
